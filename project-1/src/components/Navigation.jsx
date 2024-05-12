3e431c2e00861f9cf74331a4e0977a9a6c086d7d
saksham1742683 <sakshamjain1742683@gmail.com>
2024-05-12 23:22:18 +0530

    ecommerce-page-1st react app

---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo image" />
+      </div>
+      <ul>
+        <li>Menu</li>
+        <li>Location</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
